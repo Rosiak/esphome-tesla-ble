8b7984706038345e474c64374345c228f56d278b
PedroKTFC <giordys@gmail.com>
2025-04-28 00:00:52 +0100

    First version supporting battery level, shift state and odometer

--- components/tesla_ble_vehicle/__init__.py
+++ components/tesla_ble_vehicle/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import ble_client, binary_sensor
+from esphome.components import ble_client, binary_sensor, text_sensor, sensor
 from esphome.const import CONF_ID
 
 CODEOWNERS = ["@yoziru"]
@@ -17,6 +17,9 @@ CONF_IS_ASLEEP = "is_asleep"
 CONF_IS_UNLOCKED = "is_unlocked"
 CONF_IS_USER_PRESENT = "is_user_present"
 CONF_IS_CHARGE_FLAP_OPEN = "is_charge_flap_open"
+CONF_SHIFT_STATE = "shift_state"
+CONF_CHARGE_STATE = "charge_state"
+CONF_ODOMETER = "odometer"
 
 CONFIG_SCHEMA = (
     cv.Schema(
@@ -35,6 +38,16 @@ CONFIG_SCHEMA = (
             ).extend(),
             cv.Optional(CONF_IS_CHARGE_FLAP_OPEN): binary_sensor.binary_sensor_schema(
                 icon="mdi:ev-plug-tesla", device_class=binary_sensor.DEVICE_CLASS_DOOR
+            ).extend(),
+            cv.Optional(CONF_SHIFT_STATE): text_sensor.text_sensor_schema(
+                icon="mdi:car-shift-pattern"
+            ).extend(),
+            cv.Optional(CONF_CHARGE_STATE): sensor.sensor_schema(
+                icon="mdi:battery-medium", device_class=sensor.DEVICE_CLASS_BATTERY
+            ).extend(),
+            cv.Optional(CONF_ODOMETER): sensor.sensor_schema(
+                icon="mdi:counter", device_class=sensor.DEVICE_CLASS_DISTANCE,
+                accuracy_decimals=2, unit_of_measurement="miles"
             ).extend(),
         }
     )
@@ -70,3 +83,18 @@ async def to_code(config):
         conf = config[CONF_IS_CHARGE_FLAP_OPEN]
         bs = await binary_sensor.new_binary_sensor(conf)
         cg.add(var.set_binary_sensor_is_charge_flap_open(bs))
+
+    if CONF_SHIFT_STATE in config:
+        conf = config[CONF_SHIFT_STATE]
+        ts = await text_sensor.new_text_sensor(conf)
+        cg.add(var.set_text_sensor_shift_state(ts))
+
+    if CONF_CHARGE_STATE in config:
+        conf = config[CONF_CHARGE_STATE]
+        ss = await sensor.new_sensor(conf)
+        cg.add(var.set_sensor_charge_state(ss))
+
+    if CONF_ODOMETER in config:
+        conf = config[CONF_ODOMETER]
+        ss = await sensor.new_sensor(conf)
+        cg.add(var.set_sensor_odometer_state(ss))
