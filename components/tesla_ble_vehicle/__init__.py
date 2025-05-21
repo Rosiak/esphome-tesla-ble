7811d447d5867e867ba7aa1e7966e30b8291e74a
PedroKTFC <giordys@gmail.com>
2025-05-21 19:36:17 +0100

    Add support for ble_disconnected_min_time # Minimum time BLE must be disconnected before sensors are Unknown

--- components/tesla_ble_vehicle/__init__.py
+++ components/tesla_ble_vehicle/__init__.py
@@ -29,6 +29,7 @@ CONF_POST_WAKE_POLL_TIME = "post_wake_poll_time" # How long to poll for data aft
 CONF_POLL_DATA_PERIOD = "poll_data_period" # Normal period when polling for data when not asleep (s)
 CONF_POLL_ASLEEP_PERIOD = "poll_asleep_period" # Period to poll for data when asleep (s)
 CONF_POLL_CHARGING_PERIOD = "poll_charging_period" # Period to poll for data when charging (s)
+CONF_BLE_DISCONNECTED_MIN_TIME= "ble_disconnected_min_time" # Minimum time BLE must be disconnected before sensors are Unknwon (s)
 
 CONFIG_SCHEMA = (
     cv.Schema(
@@ -40,6 +41,7 @@ CONFIG_SCHEMA = (
             cv.Optional(CONF_POLL_DATA_PERIOD): cv.uint16_t,
             cv.Optional(CONF_POLL_ASLEEP_PERIOD): cv.uint16_t,
             cv.Optional(CONF_POLL_CHARGING_PERIOD): cv.uint16_t,
+            cv.Optional(CONF_BLE_DISCONNECTED_MIN_TIME): cv.uint16_t,
             cv.Optional(CONF_IS_ASLEEP): binary_sensor.binary_sensor_schema(
                 icon="mdi:sleep"
             ).extend(),
@@ -96,7 +98,7 @@ async def to_code(config):
 
     cg.add(var.set_vin(config[CONF_VIN]))
     cg.add(var.load_polling_parameters(config[CONF_POST_WAKE_POLL_TIME], config[CONF_POLL_DATA_PERIOD],
-           config[CONF_POLL_ASLEEP_PERIOD], config[CONF_POLL_CHARGING_PERIOD]))
+           config[CONF_POLL_ASLEEP_PERIOD], config[CONF_POLL_CHARGING_PERIOD], config[CONF_BLE_DISCONNECTED_MIN_TIME]))
 
     if CONF_IS_ASLEEP in config:
         conf = config[CONF_IS_ASLEEP]
